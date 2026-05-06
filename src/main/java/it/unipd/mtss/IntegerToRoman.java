23dfa8bacf58d1c2bbe8e48424e93a94cf9f7dea
RasiadalPoloAlessandro <ale.rasiadalpolo12@gmail.com>
2026-05-06 12:25:55 +0200

    Implementata Conversione dei primi 10 Numeri

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -11,12 +11,14 @@ public class IntegerToRoman {
 
     private static final LinkedHashMap<Integer, String> ROMAN_NUMBERS = new LinkedHashMap<>();
     static {
+        ROMAN_NUMBERS.put(10, "X");
+        ROMAN_NUMBERS.put(9, "IX");
         ROMAN_NUMBERS.put(5, "V");
         ROMAN_NUMBERS.put(4, "IV");
         ROMAN_NUMBERS.put(1, "I");
     }
 
-    private static final int NUMERO_MASSIMO_RAPPRESENTABILE = 6;
+    private static final int NUMERO_MASSIMO_RAPPRESENTABILE = 10;
 
     public static String convert(int number){
         if(number < 1 || number > NUMERO_MASSIMO_RAPPRESENTABILE){
