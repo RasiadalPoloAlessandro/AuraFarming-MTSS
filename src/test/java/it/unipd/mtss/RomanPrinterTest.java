4c1edc43aa3138a4854a54b393e064651fe901ca
frafurin <furinfrancesco@gmail.com>
2026-05-07 00:58:51 +0200

    sviluppo test stampa primi 10 numeri

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -38,4 +38,14 @@ public class RomanPrinterTest {
         // Assert
         assertEquals(expected, result);
     }
+    @Test
+    public void testPrintLetterX(){ //symbol is also limit case
+        // Arrange
+        int input = 10;
+        String expected = "__   __\n\\ \\ / /\n \\ V / \n  > <  \n / . \\ \n/_/ \\_\\";
+        // Act
+        String result = RomanPrinter.print(input);
+        // Assert
+        assertEquals(expected, result);
+    }
 }
