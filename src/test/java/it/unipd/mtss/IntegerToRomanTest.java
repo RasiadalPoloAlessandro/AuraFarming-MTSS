c93e7fcee903cb9e6503dcea0906fc734b00c50f
RasiadalPoloAlessandro <ale.rasiadalpolo12@gmail.com>
2026-05-06 11:56:15 +0200

    Aggiunto Test per la conversione dei primi 3 numeri

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,4 +1,32 @@
 package it.unipd.mtss;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class IntegerToRomanTest {
+
+    @Test
+    public void testConvertFirstNumber() {
+        // Arrange
+        int input = 1;
+
+        // Act
+        String result = IntegerToRoman.convert(input);
+
+        // Assert
+        assertEquals("I", result);
+    }
+
+    @Test
+    public void testConvertThree() {
+        // Arrange
+        int input = 3;
+
+        // Act
+        String result = IntegerToRoman.convert(input);
+
+        // Assert
+        assertEquals("III", result);
+    }
 }
