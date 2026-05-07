face3bb7afcb72544b063bc4fd714eff7f0ab798
RasiadalPoloAlessandro <ale.rasiadalpolo12@gmail.com>
2026-05-07 18:06:04 +0200

    Implementata Conversione dei primi 20 Numeri

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -18,7 +18,7 @@ public class IntegerToRoman {
         ROMAN_NUMBERS.put(1, "I");
     }
 
-    private static final int NUMERO_MASSIMO_RAPPRESENTABILE = 10;
+    private static final int NUMERO_MASSIMO_RAPPRESENTABILE = 20;
 
     public static String convert(int number){
         if(number < 1 || number > NUMERO_MASSIMO_RAPPRESENTABILE){
