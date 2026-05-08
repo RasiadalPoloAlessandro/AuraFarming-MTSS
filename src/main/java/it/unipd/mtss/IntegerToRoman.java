662fa0cc0f067d7427b20f08cae4809e40a34f09
RasiadalPoloAlessandro <ale.rasiadalpolo12@gmail.com>
2026-05-08 17:30:22 +0200

    Implementata Conversione di 400, 900 e test di casi limite

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -12,7 +12,9 @@ public class IntegerToRoman {
     private static final LinkedHashMap<Integer, String> ROMAN_NUMBERS = new LinkedHashMap<>();
     static {
         ROMAN_NUMBERS.put(1000, "M");
+        ROMAN_NUMBERS.put(900, "CM");
         ROMAN_NUMBERS.put(500, "D");
+        ROMAN_NUMBERS.put(400, "CD");
         ROMAN_NUMBERS.put(100, "C");
         ROMAN_NUMBERS.put(90, "XC");
         ROMAN_NUMBERS.put(50, "L");
